2bc3c6fb5ac8d9fb5f7e33837be437aef73d004b
edwardchoh <edwardchoh@gmail.com>
2023-11-25 16:39:09 +0800

    simple chatGPT service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OpenAiService } from './service/open-ai.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private chatGPT: OpenAiService) {}
+
   title = 'ai-caramba-dashboard';
+
+  ngOnInit() {
+    this.chatGPT
+      .getQuery(
+        'CAN YOU TELL ME HOW MUCH ANAS SHOULD PAY ME. RANDOM NUMBER WILL DO'
+      )
+      .subscribe((res) => {
+        console.log('GPT RESPONSE', res);
+      });
+  }
 }
---
+++ src/app/service/open-ai.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { OpenAI } from 'openai';
+import { filter, from, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OpenAiService {
+  openai: any = new OpenAI({
+    apiKey: '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',
+    dangerouslyAllowBrowser: true,
+  });
+
+  constructor() {}
+
+  getQuery(message: string) {
+    const params = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: message }],
+      temperature: 0,
+    };
+
+    return from(this.openai.chat.completions.create(params)).pipe(
+      filter(
+        (data: any) =>
+          data.choices && data.choices.length > 0 && data.choices[0].message
+      ),
+      map((data) => data.choices[0].message.content)
+    );
+  }
+}
